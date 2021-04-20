e1715a6c8f01367c03698295eab8e98df87af4b2
Yesman Choque <yesman.choque@gmail.com>
2021-04-20 13:40:55 -0300

    Server Side Generation

--- podcastr-next/src/pages/index.tsx
+++ podcastr-next/src/pages/index.tsx
@@ -1,7 +1,39 @@
-import { Header } from "../components/Header";
+// SPA 
+// SSR
+// SSG
 
-export default function Home() {
+import { useEffect } from "react"
+
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+//SSR
+/* export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    }
+  }
+} */
+
+//SSG
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
